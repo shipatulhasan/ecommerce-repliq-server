d0e2e0fda2d33f0f74eff63f70eb93a74c33e1bf
shipatulhasan <shipatulhasan328@gmail.com>
2023-01-05 14:12:46 +0600

    registration route created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+// const jwt = require('jsonwebtoken');
 const app = express()
 
 // middleware
@@ -10,6 +13,47 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('bubu form node')
 })
+
+
+// mongodb
+const client = new MongoClient(process.env.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+
+  const run = async()=>{
+      try{
+        const productsCollection = client.db("ecommerceReplciq").collection("products");
+        const usersCollection = client.db("ecommerceReplciq").collection("users");
+
+        app.post('/register',async(req,res)=>{
+          const user = req.body
+         
+          const query = {phone:user.phone}
+          const existingUser = await usersCollection.findOne(query)
+          if(existingUser){
+              return res.status(302).send({message:'already exist'})
+          }
+          const result = await usersCollection.insertOne(user)
+          console.log(result)
+          res.send(result)
+      })
+      }
+      
+      finally{}
+  }
+  run().catch(console.dir)
+
+
+
+
+
+
+
+
+
+
 app.listen(port,()=>{
     console.log(`listening form ${port}`)
 })
